720b7e0cfe2c257a17227ce5bc89900a7ff3dd54
DuyGTX <quangduy1997vn@gmail.com>
2023-11-14 22:08:01 +0700

    update-css-mng

--- Degrey/Controllers/CategoriesController.cs
+++ Degrey/Controllers/CategoriesController.cs
@@ -10,129 +10,107 @@ using Degrey.Models;
 
 namespace Degrey.Controllers
 {
-    public class ProductsController : Controller
+    public class CategoriesController : Controller
     {
         private DBDegreyStoreEntities db = new DBDegreyStoreEntities();
 
-        // GET: Products
-        public ActionResult Index(string category)
+        // GET: Categories
+        public ActionResult Index()
         {
-            IEnumerable<Product> products;
-
-            if (!string.IsNullOrEmpty(category))
-            {
-                // Filter products by category
-                products = db.Products.Where(p => p.Category.NameCate == category);
-            }
-            else
-            {
-                // Get all products
-                products = db.Products;
-            }
-
-            return View(products.ToList());
+            return View(db.Categories.ToList());
         }
 
-
-        // GET: Products/Details/5
+        // GET: Categories/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Products.Find(id);
-            if (product == null)
+            Category category = db.Categories.Find(id);
+            if (category == null)
             {
                 return HttpNotFound();
             }
-            return View(product);
+            return View(category);
         }
 
-        // GET: Products/Create
+        // GET: Categories/Create
         public ActionResult Create()
         {
-            ViewBag.CatID = new SelectList(db.Categories, "CatID", "NameCate");
-            ViewBag.ProID = new SelectList(db.Galleries, "ProID", "ProImage");
             return View();
         }
 
-        // POST: Products/Create
+        // POST: Categories/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ProID,ProName,CatID,Description,ProImage,Price,PriceSale,Discount,CreatedDate")] Product product)
+        public ActionResult Create([Bind(Include = "CatID,NameCate")] Category category)
         {
             if (ModelState.IsValid)
             {
-                db.Products.Add(product);
+                db.Categories.Add(category);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.CatID = new SelectList(db.Categories, "CatID", "NameCate", product.CatID);
-            ViewBag.ProID = new SelectList(db.Galleries, "ProID", "ProImage", product.ProID);
-            return View(product);
+            return View(category);
         }
 
-        // GET: Products/Edit/5
+        // GET: Categories/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Products.Find(id);
-            if (product == null)
+            Category category = db.Categories.Find(id);
+            if (category == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.CatID = new SelectList(db.Categories, "CatID", "NameCate", product.CatID);
-            ViewBag.ProID = new SelectList(db.Galleries, "ProID", "ProImage", product.ProID);
-            return View(product);
+            return View(category);
         }
 
-        // POST: Products/Edit/5
+        // POST: Categories/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProID,ProName,CatID,Description,ProImage,Price,PriceSale,Discount,CreatedDate")] Product product)
+        public ActionResult Edit([Bind(Include = "CatID,NameCate")] Category category)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(product).State = EntityState.Modified;
+                db.Entry(category).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.CatID = new SelectList(db.Categories, "CatID", "NameCate", product.CatID);
-            ViewBag.ProID = new SelectList(db.Galleries, "ProID", "ProImage", product.ProID);
-            return View(product);
+            return View(category);
         }
 
-        // GET: Products/Delete/5
+        // GET: Categories/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Product product = db.Products.Find(id);
-            if (product == null)
+            Category category = db.Categories.Find(id);
+            if (category == null)
             {
                 return HttpNotFound();
             }
-            return View(product);
+            return View(category);
         }
 
-        // POST: Products/Delete/5
+        // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Product product = db.Products.Find(id);
-            db.Products.Remove(product);
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
